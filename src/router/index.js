84feb01de82fd0c881ffaba48b9c91e4af4a7307
erlanggajatikusuma007 <erlanggajatikusuma007@gmail.com>
2021-09-03 10:41:20 +0700

    init project & install react navigation

---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+// In App.js in a new project
+
+import * as React from 'react';
+import {View, Text} from 'react-native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {SplashScreen} from '../pages';
+
+function HomeScreen() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+const Router = () => {
+  return (
+    <Stack.Navigator initialRouteName="SplashScreen">
+      <Stack.Screen
+        name="SplashScreen"
+        component={SplashScreen}
+        options={{headerShown: false}}
+      />
+    </Stack.Navigator>
+  );
+};
+
+export default Router;
