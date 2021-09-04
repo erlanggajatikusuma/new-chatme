5d1e3c575646ce8fcf17295133e1b5587763121a
erlanggajatikusuma007 <erlanggajatikusuma007@gmail.com>
2021-09-04 09:32:00 +0700

    INSTALL DRAWER 0.65 not supported must use reanimated 2 alpha'

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,6 @@ import SplashScreen from './SplashScreen';
 import Login from './Login';
 import SignUp from './SignUp';
 import ForgotPassword from './ForgotPassword';
+import Messages from './Messages';
 
-export {SplashScreen, SignUp, Login, ForgotPassword};
+export {SplashScreen, SignUp, Login, ForgotPassword, Messages};
